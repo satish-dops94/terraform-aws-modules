3b12fe2fb89556c2ef2f494e722b8fee5235153b
satish-dops94 <vsatish.94@gmail.com>
2020-10-08 13:46:01 +0530

    added backend.tf

--- modules/aws_db_instance/main.tf
+++ modules/aws_db_instance/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {}
+}
+
 data "aws_vpcs" "selected" {
   filter {
     name    = "tag:Name"
@@ -85,5 +89,4 @@ resource "aws_db_subnet_group" "default" {
   count      = length(data.aws_vpcs.selected.ids)
   name       = var.db_subnet_group_name
   subnet_ids = data.aws_subnet_ids.subnets[count.index].ids
-  tags       = var.tags
 }
